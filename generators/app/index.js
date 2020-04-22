b4a21c47fb550c78413cd764fda90d7231bae65a
wxia <wxia@dfocuspace.com>
2020-04-22 16:58:11 +0800

    fix: eslint

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,14 +1,20 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+/***
+ * @Author:xiawen
+ * @Date:2020-04-22 15:52:30
+ * @LastModifiedBy:xiawen
+ * @Last Modified time:2020-04-22 15:52:30
+ */
+
+'use strict'
+
+const Generator = require('yeoman-generator')
+const chalk = require('chalk')
+const yosay = require('yosay')
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the astounding ${chalk.red('generator-react-staging')} generator!`)
-    );
+    this.log(yosay(`Welcome to the astounding ${chalk.red('generator-react-staging')} generator!`))
 
     const prompts = [
       {
@@ -17,22 +23,19 @@ module.exports = class extends Generator {
         message: 'Would you like to enable this option?',
         default: true
       }
-    ];
+    ]
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+      this.props = props
+    })
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    this.fs.copy(this.templatePath('dummyfile.txt'), this.destinationPath('dummyfile.txt'))
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies()
   }
-};
+}
